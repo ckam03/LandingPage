fcf558e3b950aed609c53487695c6f88abaa2494
Chris <cwkamer@gmail.com>
2020-10-05 14:41:34 -0700

    cleaned up code

--- public/main.js
+++ public/main.js
@@ -1,21 +1,16 @@
-
-
 const time = document.getElementById('Time');
-greeting = document.getElementById('greeting');
-var name = document.getElementById('name');
+let greeting = document.getElementById('greeting');
+let name = document.getElementById('name');
 
 function showTime() {
     let today = new Date();
     hour = today.getHours();
     min = today.getMinutes();
-    //sec = today.getSeconds();
 
     // sets AM or PM
     const amPm = hour >= 12 ? 'PM' : 'AM';
     // 12 hour format
     hour = hour % 12 || 12;
-
-    //Time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`;
     Time.innerHTML = new Date().toLocaleTimeString();
     setTimeout(showTime, 1000);
 }
@@ -30,44 +25,49 @@ async function getPhoto() {
             },
         })
         const photoData = await response.json();
-        //console.log(photoData)
-        setPhoto(photoData)
+        console.log(photoData)
+        var unsplashImage = `url(${photoData.urls.full})`;
+        savePhoto(unsplashImage)
     }
     catch (error) {
         console.log(error)
     }   
 }
 
-function setPhoto(photoData) {
-    document.body.style.backgroundImage = `url(${photoData.urls.full})`;
+function setPhoto() {
+    unsplashImage = localStorage.getItem('bg');
+    document.body.style.backgroundImage = unsplashImage; 
 }
 
+function savePhoto(unsplashImage) {
+        localStorage.setItem('bg', unsplashImage);
+        setPhoto();
+
+}
 
 //changes background and greeting based on time of day
 function setBgGreeting() {
     let today = new Date(),
         hour = today.getHours();
+        minute = today.getMinutes();
 
     if (hour < 12) {
-        //document.body.style.backgroundImage = "url('../public/imgs/morning.jpg')";
         greeting.textContent = 'Good Morning,';
     }
+
     else if (hour < 18) {
-        //document.body.style.backgroundImage = "url('../public/imgs/field.jpg')";
+
         greeting.textContent = 'Good Afternoon,';
         document.body.style.color = 'white';
     }
     else {
-        //document.body.style.backgroundImage = "url('../public/imgs/evening.jpg')";
+
         greeting.textContent = 'Good Evening,';
         document.body.style.color = 'white';
     }
 }
-
-
-
-showTime();
 getPhoto();
+showTime();
 setBgGreeting();
 
 
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-//const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 const dotenv = require('dotenv').config();
@@ -15,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-//app.use(morgan('common'));
+
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json())
 app.use(express.static('public'))
@@ -23,16 +22,15 @@ app.use(express.static('public'))
 app.post('/photos/random', (req, res) => {
     
     unsplash.photos.getRandomPhoto({
-        query: "nature",
-        
+        query: "nature",    
     })
         .then(toJson)
         .then(json => {
+            unsplash.photos.trackDownload(json);
             res.send(json)
         })
         
 })
-
 
 app.listen(3000, () => {
     console.log('Server started on port 3000')
